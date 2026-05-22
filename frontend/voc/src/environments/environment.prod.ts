4803405ad8e921dd0bb8392dca20091540209c79
Michael Velasco <michael.velasco.amaya@gmail.com>
2026-05-22 11:35:17 -0500

    fix: update chat access token for secure communication

--- frontend/voc/src/environments/environment.prod.ts
+++ frontend/voc/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   festivosApiKey: 'fs_3mNHbhSyjhJyTtdhiKhQjtAC6LWj1MAc',
   festivosApiUrl: 'https://festivos.com.co/api/v1/festivos',
   uploadNotifyUrl: 'https://renediaz2025.app.n8n.cloud/webhook/a4784977-134a-4f09-9ea3-04c85c5ba3b7',
-  chatAccessToken: '6f3YnUiTZAuZz2rbHeNSz4uJFJCG88Oscl7SyW9wueE',
+  chatAccessToken: 'SgJhjtMN78E3EmJWrczj-qhCHY9wOovZfquv2g8e8',
   chatGetConversationsUrl: 'https://n8n.srv865978.hstgr.cloud/webhook/chat_web_connectror/get_chats',
   chatGetMessagesUrl: 'https://n8n.srv865978.hstgr.cloud/webhook/493e72f3-1aae-4dd4-92f5-84c8924e812d/chat_web_connectror/get_chats',
   chatSendMessageUrl: 'https://n8n.srv865978.hstgr.cloud/webhook/chat_web_connectror/chat'
